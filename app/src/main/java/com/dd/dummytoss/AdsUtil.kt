ec74a05c9ed35b0628f2d0a0aad6f341bd05f4ba
01ritzs <01ritzs@gmail.com>
2021-05-12 23:58:22 +0530

    Addition of AdsUtils class for interstitial ads

---
+++ app/src/main/java/com/dd/dummytoss/AdsUtil.kt
@@ -0,0 +1,69 @@
+package com.dd.dummytoss
+
+import android.app.Activity
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.initialization.InitializationStatus
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+
+class AdsUtils private constructor() {
+    private val adRequest = AdRequest.Builder().build()
+    private var adInterstitial: InterstitialAd? = null
+
+    fun initMobileAds(context: Context?) {
+        MobileAds.initialize(context) { initializationStatus: InitializationStatus? ->
+            isMobileAddInitialized = true
+        }
+    }
+
+    /**
+     * Function to load interstitial ad
+     *
+     * @param context Context
+     */
+    fun loadInterstitial(context: Context) {
+        if (isMobileAddInitialized) {
+            val adInterstitialId = context.resources.getString(R.string.interstitial_id)
+            InterstitialAd.load(
+                context,
+                adInterstitialId,
+                adRequest,
+                object : InterstitialAdLoadCallback() {
+                    override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                        adInterstitial = interstitialAd
+                    }
+
+                    override fun onAdFailedToLoad(loadAdError: LoadAdError) {
+                        adInterstitial = null
+                    }
+                })
+        }
+    }
+
+    fun showInterstitialAd(activity: Activity) {
+        if (adInterstitial != null) {
+            adInterstitial!!.show(activity)
+        } else {
+            loadInterstitial(activity)
+        }
+    }
+
+    companion object {
+        @JvmField
+        var instance: AdsUtils? = null
+        private var isMobileAddInitialized = false
+
+        @JvmStatic
+        fun getInstance(): AdsUtils? {
+            if (instance == null) {
+                instance = AdsUtils()
+            }
+            return instance
+        }
+    }
+}
--- app/src/main/java/com/dd/dummytoss/MainScreen.kt
+++ app/src/main/java/com/dd/dummytoss/MainScreen.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import android.graphics.drawable.AnimationDrawable
 import android.util.Log
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.*
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,10 +20,9 @@ class MainScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MobileAds.initialize(this) {}
         mAdView = findViewById(R.id.adBanner)
+        AdsUtils.getInstance()?.initMobileAds(this)
         adBannerLoading()
-        adInterstitialLoading()
 
         val animDrawable = rlBackground.background as AnimationDrawable
         animDrawable.setEnterFadeDuration(4000)
@@ -40,31 +36,17 @@ class MainScreen : AppCompatActivity() {
         mAdView.loadAd(adRequest)
     }
 
-    private fun adInterstitialLoading() {
-        var adRequest = AdRequest.Builder().build()
-        InterstitialAd.load(this, "ca-app-pub-3949256999942544/1033173712", adRequest, object :
-            InterstitialAdLoadCallback() {
-            override fun onAdFailedToLoad(p0: LoadAdError) {
-                Log.d(TAG, p0?.message)
-                mInterstitialAd = null
-            }
-
-            override fun onAdLoaded(p0: InterstitialAd) {
-                Log.d(TAG, "Ad was Loaded.")
-                mInterstitialAd = p0
-            }
-        })
+    override fun onResume() {
+        super.onResume()
+        AdsUtils.getInstance()?.loadInterstitial(this)
     }
+
+
 
     private fun onCoinToss() {
         ivCoin.setOnClickListener {
-            if (mInterstitialAd != null) {
-                mInterstitialAd?.show(this)
-            } else {
-                Log.d(TAG, "The interstitial ad wasn't ready yet.")
-            }
+            AdsUtils.getInstance()?.showInterstitialAd(this)
             val randomNumber = (1..2).random()
-
             if (randomNumber == 1) {
                 flipTheCoin(R.drawable.ic_head, "Heads")
             } else {
@@ -84,15 +66,7 @@ class MainScreen : AppCompatActivity() {
             ivCoin.isClickable = true
         }.start()
     }
-
 }
 
-//    private fun adUtils() {
-//        if (mInterstitialAd.isLoaded) {
-//            mInterstitialAd.show()
-//        } else {
-//            onCoinToss()
-//        }
-//    }
 
 
